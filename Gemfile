a93bef98357c846798fda71af975acdbaf497899
pitr12 <peter.dubec93@gmail.com>
2014-07-29 11:49:19 +0200

    Initialize

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ gem 'spring',        group: :development
 # gem 'debugger', group: [:development, :test]
 
 gem 'nokogiri'
+gem 'faraday'
+gem 'bootstrap-sass', '2.3.2.0'
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,18 @@
 class Document < ActiveRecord::Base
 has_many :documents_heuristics
 has_many :heuristics, :through => :documents_heuristics
+has_one :crz_document_detail
+has_many :attachments
+
+def to_indexable
+  {
+  customer: crz_document_detail.customer,
+  content: attachments.map {|a| a.pages.map(&:text) }.flatten.join(' '),
+  supplier: crz_document_detail.supplier,
+  price: (crz_document_detail.contracted_amount).to_f,
+  date: (crz_document_detail.published_on).to_date
+  }
+end
+
+
 end
